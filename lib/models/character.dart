9a563e514099150972f170697668e59e30c54bb5
hippieperm <leess0133@gmail.com>
2025-03-20 11:10:24 +0900

    Character 클래스에 속성 추가: 이름, 체력, 공격력, 방어력 및 아이템 사용 여부를 포함한 생성자 정의

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,3 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class Character {
-  
+  String name;
+  int health;
+  int attack;
+  int defense;
+  bool itemUsed = false;
+
+  Character({
+    required this.name,
+    required this.health,
+    required this.attack,
+    required this.defense,
+  });
 }

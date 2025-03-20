cd81efcd23da744e39c04e41df904783259c76ac
hippieperm <leess0133@gmail.com>
2025-03-21 03:08:58 +0900

    몬스터 클래스의 attackCharacter 메서드 수정: 데미지 계산 로직을 개선하고, 캐릭터의 체력을 감소시키는 기능 및 방어력 증가 로직을 추가함

--- lib/models/monster.dart
+++ lib/models/monster.dart
@@ -15,7 +15,19 @@ class Monster {
   });
 
   void attackCharacter(Character character) {
-    int demage = attack - character.defense;
-    if (demage < 0) demage = 0;
+    int damage = attack - character.defense;
+    if (damage < 0) damage = 0;
+
+    character.health -= damage;
+    if (character.health < 0) character.health = 0;
+
+    print('$name이(가) ${character.name}에게 $damage 데미지를 입혔습니다!');
+
+    // 3턴마다 방어력 증가
+    turnCount++;
+    if (turnCount % 3 == 0) {
+      defense += 2;
+      print('$name의 방어력이 증가했습니다! 현재 방어력: $defense');
+    }
   }
 }

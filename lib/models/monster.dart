6cedd716c8bcdf01f0ea0a15346977a439c6cb3c
hippieperm <leess0133@gmail.com>
2025-03-20 11:15:58 +0900

    Monster 클래스에 공격 메서드 추가: 캐릭터를 공격하는 기능 구현

--- lib/models/monster.dart
+++ lib/models/monster.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:text_dungeon/models/character.dart';
+
 class Monster {
   String name;
   int health;
@@ -11,4 +13,9 @@ class Monster {
     required this.health,
     required this.attack,
   });
+
+  void attackCharacter(Character character) {
+    int demage = attack - character.defense;
+    if (demage < 0) demage = 0;
+  }
 }

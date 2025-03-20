8403e18646d9ed37674481dfb86986309b697c91
hippieperm <leess0133@gmail.com>
2025-03-21 03:04:55 +0900

    Character 클래스에 몬스터 공격 메서드 추가: 몬스터에게 공격하는 로직을 구현하고, 데미지 계산 및 상태 출력 기능을 추가함

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:text_dungeon/models/monster.dart';
+
 class Character {
   String name;
   int health;
@@ -12,6 +14,16 @@ class Character {
     required this.attack,
     required this.defense,
   });
+
+  void attackMonster(Monster monster) {
+    int damage = attack - monster.defense;
+    if (damage < 0) damage = 0;
+
+    monster.health -= damage;
+    if (monster.health < 0) monster.health = 0;
+
+    print('$name이(가) ${monster.name}에게 $damage 데미지를 입혔습니다!');
+  }
 
   void showStatus() {
     print('[$name] 체력: $health | 공격력: $attack | 방어력: $defense');

377742a6bab5dbcf8492681dc8e2498e9e0a1f07
hippieperm <leess0133@gmail.com>
2025-03-21 03:08:09 +0900

    Character 클래스에 useItem 메서드 추가: 아이템 사용 로직을 구현하고, 공격력을 일시적으로 증가시키는 기능을 추가함

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -26,12 +26,30 @@ class Character {
     print('$name이(가) ${monster.name}에게 $damage 데미지를 입혔습니다!');
   }
 
-  void defend(Monster monster) { //캐릭터 방어로직
+  void defend(Monster monster) {
+    //캐릭터 방어로직
     int monsterDamage = monster.attack - defense;
     if (monsterDamage < 0) monsterDamage = 0;
 
     health += monsterDamage;
     print('$name이(가) 방어하여 $monsterDamage만큼 체력을 회복했습니다!');
+  }
+
+  void useItem() {
+    // 아이템사용
+    if (!itemUsed) {
+      itemUsed = true;
+      int originalAttack = attack;
+      attack *= 2;
+      print('$name이(가) 특수 아이템을 사용하여 공격력이 두 배로 증가했습니다! 현재 공격력: $attack');
+
+      // 아이템 효과는 한 턴만 지속
+      Future.delayed(const Duration(milliseconds: 100), () {
+        attack = originalAttack;
+      });
+    } else {
+      print('이미 아이템을 사용했습니다!');
+    }
   }
 
   void showStatus() {
